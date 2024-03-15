493ae01b14d801669364eaf9637bb749aee43fee
yizhu zong <agnes.lain@gmail.com>
2024-03-15 12:04:33 +0100

    make change for new computer with new IP access

--- app/controllers/api/v1/ping_controller.rb
+++ app/controllers/api/v1/ping_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::PingController < Api::V1::BaseController
-  # skip_before_action :authenticate_user!
-  skip_after_action :verify_policy_scoped
+  skip_before_action :authenticate_devise_api_token!
+  skip_after_action :verify_policy_scoped, :raise => false
 
   def index
     render json: {message: "pong"}
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '192.168.1.176:8081/' # later change to the domain of the frontend app
+    origins '192.168.1.53:8081/' # later change to the domain of the frontend app
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head],
